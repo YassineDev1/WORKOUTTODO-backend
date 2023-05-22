c54835e2044c7b82099acd16bad63dc52cdf0155
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-22 21:31:09 +0100

    create Workout Model

---
+++ models/Workout.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  reps: { type: Number, required: true },
+  load: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
