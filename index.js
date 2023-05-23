ffbc9f4b297e051712cc69e5534fbc613645f58a
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-23 12:51:17 +0100

    add cors middleware

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+app.use(function (req, res, next){
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,HEAD, OPTIONS, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requeste-With, Content-Type, Accept, Authorization" );
+  next();
+})
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
