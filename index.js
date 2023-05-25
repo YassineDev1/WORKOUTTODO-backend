27e40cc058706f8180738269626f3b6e5b3feb28
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-05-25 21:32:18 +0100

    add origins middleware

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use(
     credentials: true
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // or specify the origin(s) of your client application
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 
 mongoose
